e0afe7c95f577c738831752514d4e616b8856608
Irshad Buchh <ibuchh@gmail.com>
2022-05-14 19:14:42 -0500

    initial commit

---
+++ terraform/iam.tf
@@ -0,0 +1,66 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# APPRUNNER IAM Role
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_iam_role" "apprunner-service-role" {
+  name               = "${var.apprunner-service-role}AppRunnerECRAccessRole"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.apprunner-service-assume-policy.json
+}
+
+data "aws_iam_policy_document" "apprunner-service-assume-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["build.apprunner.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "apprunner-service-role-attachment" {
+  role       = aws_iam_role.apprunner-service-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+
+resource "aws_iam_role" "apprunner-instance-role" {
+  name = "${var.apprunner-service-role}AppRunnerInstanceRole"
+  path = "/"
+  assume_role_policy = data.aws_iam_policy_document.apprunner-instance-assume-policy.json
+}
+
+resource "aws_iam_policy" "Apprunner-policy" {
+  name = "Apprunner-getSSM"
+  policy = data.aws_iam_policy_document.apprunner-instance-role-policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "apprunner-instance-role-attachment" {
+  role = aws_iam_role.apprunner-instance-role.name
+  policy_arn = aws_iam_policy.Apprunner-policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "apprunner-instance-role-xray-attachment" {
+  role = aws_iam_role.apprunner-instance-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess" 
+}
+
+data "aws_iam_policy_document" "apprunner-instance-assume-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["tasks.apprunner.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "apprunner-instance-role-policy" {
+  statement {
+    actions = ["ssm:GetParameter"]
+    effect = "Allow"
+    resources = ["arn:aws:ssm:*:${data.aws_caller_identity.current.account_id}:parameter${data.aws_ssm_parameter.dbpassword.name}"]
+  }
+}
---
+++ terraform/services.tf
@@ -0,0 +1,49 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# APPRUNNER Service
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_apprunner_service" "service" {
+  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.auto-scaling-config.arn
+  service_name                   = "apprunner-petclinic"
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.apprunner-service-role.arn
+    }
+    image_repository {
+      image_configuration {
+        port = var.container_port
+        runtime_environment_variables = {
+          "AWS_REGION" : "${var.aws_region}",
+          "spring.datasource.username" : "${var.db_user}",
+          "spring.datasource.initialization-mode" : var.db_initialize_mode,
+          "spring.profiles.active" : var.db_profile,
+          "spring.datasource.url" : "jdbc:mysql://${aws_db_instance.db.address}/${var.db_name}"
+        }
+      }
+      image_identifier      = "${data.aws_ecr_repository.image_repo.repository_url}:latest"
+      image_repository_type = "ECR"
+    }
+  }
+  
+  network_configuration {
+    egress_configuration {
+      egress_type       = "VPC"
+      vpc_connector_arn = aws_apprunner_vpc_connector.connector.arn
+    }
+  }
+  
+  instance_configuration {
+    instance_role_arn = aws_iam_role.apprunner-instance-role.arn
+  }
+  depends_on = [aws_iam_role.apprunner-service-role, aws_db_instance.db, aws_route_table.private-route-table, null_resource.petclinic_springboot]
+}
+
+resource "aws_apprunner_vpc_connector" "connector" {
+  vpc_connector_name = "petclinic_vpc_connector"
+  subnets            = aws_subnet.private[*].id
+  security_groups    = [aws_security_group.db-sg.id]
+}
+
+output "apprunner_service_url" {
+  value = "https://${aws_apprunner_service.service.service_url}"
+}
