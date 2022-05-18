0e875b54146a5d79a777a00e7985dec1a24fed38
Galen Dunkleberger <awsgalen@amazon.com>
2022-05-17 20:57:28 -0400

    updates to petclinic

--- terraform/services.tf
+++ terraform/services.tf
@@ -14,10 +14,10 @@ resource "aws_apprunner_service" "service" {
         port = var.container_port
         runtime_environment_variables = {
           "AWS_REGION" : "${var.aws_region}",
-          "spring.datasource.username" : "${var.db_user}",
-          "spring.datasource.initialization-mode" : var.db_initialize_mode,
-          "spring.profiles.active" : var.db_profile,
-          "spring.datasource.url" : "jdbc:mysql://${aws_db_instance.db.address}/${var.db_name}"
+          "SPRING_DATASOURCE_USERNAME" : "${var.db_user}",
+          "SPRING_DATASOURCE_INITIALIZATION_MODE" : var.db_initialize_mode,
+          "SPRING_PROFILES_ACTIVE" : var.db_profile,
+          "SPRING_DATASOURCE_URL" : "jdbc:mysql://${aws_db_instance.db.address}/${var.db_name}"
         }
       }
       image_identifier      = "${data.aws_ecr_repository.image_repo.repository_url}:latest"
